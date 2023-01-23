2c3b2fd5f80877e99fb43343f933f91a72d8addc
Manoj Kumar <manojsfstm5@gmail.com>
2023-01-23 22:40:37 +0530

    final 2

--- admin/addproduct.js
+++ admin/addproduct.js
@@ -72,23 +72,23 @@ function displayData(bag) {
   console.log(bag)
   bag.forEach((ele) => {
     let div = document.createElement("div");
-    let img = document.createElement("img");
-    img.setAttribute("src", ele.Img_ulr);
-    let Title = document.createElement("p");
-    Title.innerText = ele.Title;
-    let Brand = document.createElement("h5");
-    Brand.innerText = ele.Brand;
-    let MRP = document.createElement("h3");
-    MRP.innerHTML = `<h5>&#8377${ele.Mrp}</h5>`;
-   
-    // MRP.innerText=ele.MRP
-    let price = document.createElement("h5");
-    price.innerHTML = `<h4>&#8377; ${ele.Price}</h4>`
-    price.style.textDecoration=`line-through`
-    let Id=document.createElement("p")
-    Id.innerText=ele._id
-    div.append(img, Title, Brand, MRP, price,Id);
-    document.querySelector(".fetchdata").append(div);
+      let img = document.createElement("img");
+      img.setAttribute("src", ele.Img_ulr);
+      let Title = document.createElement("p");
+      Title.innerText = ele.Title;
+      let Brand = document.createElement("h5");
+      Brand.innerText = ele.Brand;
+      let MRP = document.createElement("h3");
+      MRP.innerHTML = `<h5>&#8377; ${ele.Mrp}</h5>`;
+     
+      // MRP.innerText=ele.MRP
+      let price = document.createElement("h5");
+      price.innerHTML = `<h4>&#8377; ${ele.Price}</h4>`
+      price.style.textDecoration=`line-through`
+      let Id=document.createElement("p")
+      Id.innerText=ele._id
+      div.append(img, Title, Brand, MRP, price,Id);
+      document.querySelector(".fetchdata").append(div)
   });
 }
 
